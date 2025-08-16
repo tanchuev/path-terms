645802d73d184e203b630fbfdfe662446db4992f
Marat Taychinov <tanchuev@gmail.com>
2025-08-16 10:24:49 +0500

    savig state

--- app.js
+++ app.js
@@ -51,6 +51,20 @@ class SimpleRouter {
                     <header class="header">
                         <h1 class="game-title">Path</h1>
                         <p class="game-subtitle">Zen. Flow. Puzzle.</p>
+                        
+                        <div class="glowing-orb"></div>
+                        
+                        <div class="store-buttons">
+                            <a href="https://apps.apple.com/app/idYOUR_APP_ID" target="_blank" class="store-button">
+                                <img src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg" alt="Download on the App Store">
+                            </a>
+                            <a href="https://play.google.com/store/apps/details?id=com.tanchuev.path" target="_blank" class="store-button google">
+                                <img src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" alt="Get it on Google Play">
+                            </a>
+                            <a href="https://apps.rustore.ru/app/YOUR_APP_ID" target="_blank" class="store-button rustore">
+                                <img src="https://www.rustore.ru/help/icons/logo-color-dark.png" alt="Доступно в RuStore">
+                            </a>
+                        </div>
                     </header>
                     
                     <div class="main-content">
@@ -58,27 +72,13 @@ class SimpleRouter {
                     </div>
                     
                     <footer class="footer">
-                        <div class="store-buttons">
-                            <a href="https://apps.apple.com/app/idYOUR_APP_ID" target="_blank" class="store-button">
-                                <img src="https://developer.apple.com/app-store/marketing/guidelines/images/badge-download-on-the-app-store.svg" alt="Download on the App Store">
-                            </a>
-                            <a href="https://play.google.com/store/apps/details?id=com.tanchuev.path" target="_blank" class="store-button">
-                                <img src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" alt="Get it on Google Play">
-                            </a>
-                            <a href="https://apps.rustore.ru/app/YOUR_APP_ID" target="_blank" class="store-button rustore">
-                                <img src="https://www.rustore.ru/help/icons/logo-color-dark.png" alt="Доступно в RuStore">
-                            </a>
-                        </div>
-                        
-                        <div class="glowing-orb"></div>
-                        
                         <div class="footer-links">
                             <button class="footer-link" onclick="router.navigate('/feedback')">
                                 Обратная связь
                             </button>
-                            <button class="footer-link" onclick="router.navigate('/privacy')">
+                            <p class="privacy-text" onclick="router.navigate('/privacy')">
                                 Политика конфиденциальности
-                            </button>
+                            </p>
                         </div>
                     </footer>
                 </div>
